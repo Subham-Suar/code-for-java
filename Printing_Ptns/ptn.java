a5f672cadd766b39bc1211d1db577aeb2046b4f1
Subham-Suar <subhamsuar911609@gmail.com>
2026-01-29 23:45:34 +0530

    star patterns

--- Printing_Ptns/ptn.java
+++ Printing_Ptns/ptn.java
@@ -1,22 +1,37 @@
-package Printing_Ptns;
+ package Printing_Ptns;
 
-    class Ptn {
+
+
+class ptn9{
         public static void main(String[] args) {
                 //for row
-               for(int i=0; i<5; i++){
+        for(int i=0; i<5; i++){
                 //for space
-                for(int  j=0; j<5-i-1; j++){
-                        System.out.print(" ");}
+        for(int  j=0; j<5-i-1; j++){
+                 System.out.print(" ");}
                  //for star
-                        for(int k=0; k<2*i+1; k++){
-                                System.out.print("*");}
+        for(int k=0; k<2*i+1; k++){
+                System.out.print("*");}
                  //for space
-                                for (int  l=0; l<5-i-1; l++) {
-                                      System.out.print(" ");  
+        for (int  l=0; l<5-i-1; l++) {
+              System.out.print(" ");  
                                 }
+     
                                 System.out.println(); 
                         }
+        for(int a=4; a>=0; a-- ){ 
+                //for space
+        for(int  j=0; j<5-a-1; j++){
+                 System.out.print(" ");}
+                 //for star
+        for(int k=0; k<2*a+1; k++){
+                System.out.print("*");}
+                 //for space
+        for (int  l=0; l<5-a-1; l++) {
+              System.out.print(" ");  
+                                }
+                          System.out.println();         
                 }
-               
-               } 
+}
+}
 

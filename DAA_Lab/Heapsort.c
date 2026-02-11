37c7ea498b0cf36702aee793144662bc71caf3c0
Subham-Suar <subhamsuar911609@gmail.com>
2026-02-11 10:01:47 +0530

    sortinng technique

---
+++ DAA_Lab/Heapsort.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int heapify(int [],,int,int);
+int hs(int[],int);
+int main()
+{
+    int a[100],i,n;
+    printf("enter the number of elements to array:");
+    scanf("%d",&n);
+    printf("enter the elements to array:");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("before sorting array elements are :\n")
+    for(i)
+    
+}

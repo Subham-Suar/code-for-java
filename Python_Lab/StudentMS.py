9fcae7d182854a379aa209fa97f3b85bd9c64233
Subham-Suar <subhamsuar911609@gmail.com>
2026-02-12 12:17:25 +0530

    code-for -python

---
+++ Python_Lab/StudentMS.py
@@ -0,0 +1,9 @@
+#Students marksheet using dicitionary
+students={}
+student["name"] = input("enter the name:")
+student["rollno"] = input("enter roll  number:")
+student["math"] = input("enterthe marks of math:")
+student["english"] = input("enter the marks of english:")
+student["history"] = input("enter the marks of history:")
+student["chemistry"] = input("enter the marks of chemistry:")
+
